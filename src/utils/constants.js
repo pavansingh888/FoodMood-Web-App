cad40bd63aeffd04029c0c553f97c6499e13ff5c
Pavan Singh <psinghp888@gmail.com>
2024-12-20 05:48:40 +0530

    CORS error Fixed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-// export const LOGO_URL = 'https://cdn.dribbble.com/users/1635051/screenshots/4291569/socio_curry_logo-01.png';
-export const FETCH_RES_URL = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
-// export const FETCH_RES_URL = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
-export const FETCH_RESMENU_URL="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
+
+export const FETCH_RES_URL = 'https://cors-anywhere.herokuapp.com/www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+
+export const FETCH_RESMENU_URL="https://cors-anywhere.herokuapp.com/www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
 export const PLATFORM_FEE = 10;
 export const PACKAGING_CHARGE = 30;
 export const CART_IMG_CDN_URL= "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_100,h_100,c_fill/"
